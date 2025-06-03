552fa347eaa4c4bee7513cd1add68b0baa4120a6
Antoine F Moevus <2613844+antmoev@users.noreply.github.com>
2025-06-03 18:57:55 -0400

    :tada: My 1st component

---
+++ src/app/secret/page.js
@@ -0,0 +1,13 @@
+import Test from "@/components/test.js"
+
+export default function Secret() {
+
+    return (
+        <>
+            <h1>Allo 2</h1>
+            <p>Code secret : 12345!</p>
+            <Test></Test>
+        </>
+    )
+
+}
