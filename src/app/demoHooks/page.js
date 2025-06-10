2803c2a9c9243040a49ec196abcc679907545515
Antoine F Moevus <2613844+antmoev@users.noreply.github.com>
2025-06-10 18:55:08 -0400

    👍 Show an exemple of useState and useEffect

---
+++ src/app/demoHooks/page.js
@@ -0,0 +1,50 @@
+"use client"
+
+import {useState, useEffect} from 'react'
+
+
+function Home() {
+
+    useEffect(() => {
+        console.log(`yoyoyo je suis monté`);
+
+        // Executé quand le composant va être détruit :
+        return () => {
+            console.log(" composant je suis démonté")
+        }
+    }, []);
+
+
+    return (
+        <h1> yoyoyo</h1>
+    )
+}
+
+
+export default function demoHook() {
+
+    const [counter, setCounter] = useState(null)
+    const [msg, setMsg] = useState(null)
+
+    const [isAuth, setIsAuth] = useState(false)
+
+
+    useEffect(() => {
+        console.log(`La valeur de count a changé : ${counter}`);
+
+
+    }, []);
+
+    return (<>
+        <main className="m-9">
+            <h2>Compteur</h2>
+            {counter && <p>Valeur : {counter}</p> || <p>Waiting...</p>}
+            <button className={"m-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"}
+                    onClick={() => {
+                        setCounter(counter + 1);
+                    }}>Ajouter
+            </button>
+            {counter === 3 && <Home></Home>}
+        </main>
+    </>)
+}
