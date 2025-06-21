5ed60c85d22e96d013f6e615efab3ecb920e6844
Antoine F Moevus <2613844+antmoev@users.noreply.github.com>
2025-06-21 11:10:34 -0400

    Add some dynamic navigation

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,41 @@
+"use client"
+
 import Image from "next/image";
 import Link from "next/link";
+import {useRouter} from "next/navigation";
+
+import {useState} from "react";
 
 export default function Home() {
   const containerGridStyle = "grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20  "
   const containerFontStyle = "font-[family-name:var(--font-geist-sans)]"
 
+  const [counter, setCounter] = useState(null)
+
+  const router = useRouter()
+
+  if (counter > 5) {
+    router.push("/contact")
+  }
+
   return (
     <div className={containerGridStyle + " " + containerFontStyle}>
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <ol>
-          <li className={"underline"}><Link href="/contact">Page Contact</Link></li>
           <li className={"underline font-bold"}><a href="/contact">Page Contact</a></li>
+          <li className={"underline"}><Link href="/contact">Page Contact</Link></li>
           <li><Link href="/contact">Page Contact</Link></li>
         </ol>
 
+        <button className={"m-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"}
+                onClick={() => {
+                  setCounter(counter + 1);
+                }}>Ajouter 1 à : {counter}
+        </button>
+
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
+        <Link
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
           target="_blank"
@@ -30,7 +49,7 @@ export default function Home() {
             height={16}
           />
           Learn
-        </a>
+        </Link>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
